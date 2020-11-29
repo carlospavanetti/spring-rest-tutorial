205aad219acc3aecb28feaff1c8043e98adc4c71
Carlos Prado <carlos.prado@portalsolar.com.br>
2020-11-29 17:15:20 -0300

    Add employee model

---
+++ src/main/java/com/tutorial/resttutorial/Employee.java
@@ -0,0 +1,25 @@
+package com.tutorial.resttutorial;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String name;
+    @NonNull
+    private String role;
+}
---
+++ src/main/java/com/tutorial/resttutorial/LoadDatabase.java
@@ -0,0 +1,19 @@
+package com.tutorial.resttutorial;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class LoadDatabase {
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
+}
